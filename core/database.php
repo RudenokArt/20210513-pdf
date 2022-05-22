7529d45b4c732ac1491803c733fa5e8b5d8c15d5
RudenokArt <rudenokart@yandex.ru>
2022-05-22 21:30:38 +0300

    save

--- core/database.php
+++ core/database.php
@@ -7,13 +7,13 @@ class Database {
   public static function db_connect () {
     $host='localhost';
     
-    $log='o918458x_db';
-    $pas='o918458x_pas';
-    $db='o918458x_db';
+    // $log='o918458x_db';
+    // $pas='o918458x_pas';
+    // $db='o918458x_db';
 
-    // $log = "e25532i4_db"; 
-    // $pas = "beget_DB-1"; 
-    // $db = 'e25532i4_db';
+    $log = "root"; 
+    $pas = "root"; 
+    $db = 'pdf_pladas';
 
     $link=mysqli_connect($host,$log,$pas,$db);
     return $link;
